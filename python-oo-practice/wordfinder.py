b027414880696d86a3b9661e28de51c366bd9cc3
crpkry13i <ryleinathaniel@gmail.com>
2021-06-08 09:25:23 -0500

    uploading python oo practice

---
+++ python-oo-practice/wordfinder.py
@@ -0,0 +1,48 @@
+"""Word Finder: finds random words from a dictionary."""
+
+import random
+
+class WordFinder:
+    """Finding random words from dictionary."""
+
+    def __init__(self, path):
+        """Read dictionary and reports # of items read."""
+
+        dict_file = open(path)
+
+        self.words = self.parse(dict_file)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """Parse dict_file to list of words."""
+
+        return [w.strip() for w in dict_file]
+
+    def random(self):
+        """Return random word."""
+
+        return random.choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments.
+    
+    >>> swf = SpecialWordFinder("complex.txt")
+    3 words read
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+    """
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words, skipping blanks/comments."""
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
