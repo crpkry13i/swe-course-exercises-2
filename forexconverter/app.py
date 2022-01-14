52cf4861446d325b2b308d72db622680b857a304
crpkry13i <ryleinathaniel@gmail.com>
2022-01-14 01:06:18 -0600

    Forex Converter

--- forexconverter/app.py
+++ forexconverter/app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, render_template, flash, session, redirect, url
 from forex_python.converter import CurrencyRates
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretkey"
 
 c = CurrencyRates()
-fx_rate = c.get_rates('USD')
+fx_rates = c.get_rates('USD')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,8 +21,8 @@ def forex_convert():
     if forex_from == 'USD' and forex_to == 'USD':
         return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount)
     elif forex_from == 'USD' and forex_to != 'USD':
-        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount * fx_rate[forex_to])
+        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount * fx_rates[forex_to])
     elif forex_from != 'USD' and forex_to == 'USD':
-        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount / fx_rate[forex_from])
+        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount / fx_rates[forex_from])
     else:
-        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount / fx_rate[forex_from] * fx_rate[forex_to])
+        return render_template('result.html', forex_from=forex_from, forex_to=forex_to, amount=amount / fx_rates[forex_from] * fx_rates[forex_to])
